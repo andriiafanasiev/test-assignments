68e44c46090f8625cb303c429e9d8eb894021452
andriiafanasiev <andreynoobroo@gmail.com>
2025-06-06 18:11:17 +0200

    All files via uploaded

---
+++ js/script.js
@@ -0,0 +1,7 @@
+new fullpage('#fullpage', {
+    autoScrolling: true,
+    scrollHorizontally: false,
+    navigation: true, // буліти справа
+    keyboardScrolling: true,
+    // інші опції за бажанням
+});
